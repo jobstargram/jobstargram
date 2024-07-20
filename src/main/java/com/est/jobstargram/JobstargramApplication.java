12dd3060284ce22c417ce0876ba8037946a6a89a
JeongSeok <ljs0429777@gmail.com>
2024-07-20 15:09:56 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/est/jobstargram/JobstargramApplication.java
@@ -0,0 +1,13 @@
+package com.est.jobstargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobstargramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobstargramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/est/jobstargram/JobstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.jobstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobstargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
